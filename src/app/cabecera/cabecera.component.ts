91124e0241e8c1b4199be3a7c550fcf9680fcae7
mininekos <manuel2g2m@gmail.com>
2022-10-28 13:55:13 +0200

    terminado

--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cabecera',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabeceraComponent implements OnInit {
 
-  constructor() { }
+  public imagenBase: String
+  public estado: Boolean
+  @ViewChild('ImagenJS') imgJS!: ElementRef;
+  constructor() { 
+    this.imagenBase="../assets/Img/clank.png";
+    this.estado=true;
+  }
 
   ngOnInit(): void {
   }
 
+  CambioAngular() {
+    this.imagenBase="../assets/Img/ratchet.png";
+  }
+
+  CambioJS() {
+    this.imgJS.nativeElement.src="../assets/Img/ratchet.png";
+  }
+
+  CambioEstado(){
+    this.estado=!this.estado;
+  }
 }
