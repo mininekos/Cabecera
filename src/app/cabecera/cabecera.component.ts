c9d04e321fa4c6a0478fb1645083636dae50d376
mininekos <manuel2g2m@gmail.com>
2022-11-02 12:54:39 +0100

    Pequeña mejora

--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -9,21 +9,40 @@ export class CabeceraComponent implements OnInit {
 
   public imagenBase: String
   public estado: Boolean
+  public cambioAngular: Boolean
+  public cambioJS: Boolean
   @ViewChild('ImagenJS') imgJS!: ElementRef;
   constructor() { 
     this.imagenBase="../assets/Img/clank.png";
     this.estado=true;
+    this.cambioAngular=true;
+    this.cambioJS=true;
   }
 
   ngOnInit(): void {
   }
 
   CambioAngular() {
-    this.imagenBase="../assets/Img/ratchet.png";
+    if(this.cambioAngular==true){
+      this.imagenBase="../assets/Img/ratchet.png";
+      this.cambioAngular=false;
+    }
+    else{
+      this.imagenBase="../assets/Img/clank.png";
+      this.cambioAngular=true;
+    }
   }
 
   CambioJS() {
     this.imgJS.nativeElement.src="../assets/Img/ratchet.png";
+    if(this.cambioJS==true){
+      this.imgJS.nativeElement.src="../assets/Img/ratchet.png";
+      this.cambioJS=false;
+    }
+    else{
+      this.imgJS.nativeElement.src="../assets/Img/clank.png";
+      this.cambioJS=true;
+    }
   }
 
   CambioEstado(){
